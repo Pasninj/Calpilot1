aa6980639ead08f6a6db27c1d82fd977e1cca538
Pasninj <pasninjaxxbg@gmail.com>
2025-08-21 18:14:28 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Installer les dépendances Python
 pip install -r requirements.txt
-
-# Installer Chromium localement pour l'utilisateur
-PLAYWRIGHT_BROWSERS_PATH=~/.cache/ms-playwright playwright install chromium
